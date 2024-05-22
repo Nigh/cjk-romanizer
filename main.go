0f13095f87d372e40a232fbf928b375f01ede02e
xianii <xianyi.xia@outlook.com>
2024-05-23 00:47:41 +0800

    add color, separator

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/Nigh/transliterate/pkg/transliterate"
+	"github.com/logrusorgru/aurora/v4"
 )
 
 var (
@@ -18,6 +19,7 @@ var (
 	isDry       bool
 	skipComfirm bool
 	isSilent    bool
+	separator   string
 )
 var trans func(string) string
 
@@ -50,20 +52,17 @@ func (a FilePaths) Less(i, j int) bool {
 }
 
 var file2Rename FilePaths
+var colorize *aurora.Aurora
 
 func init() {
 	flag.BoolVar(&help, "help", false, "help")
 	flag.BoolVar(&isDry, "dry", false, "run without actually rename files")
 	flag.BoolVar(&skipComfirm, "y", false, "skip comfirm")
-	flag.BoolVar(&isSilent, "s", false, "no output print")
+	flag.BoolVar(&isSilent, "s", false, "silence output")
 
+	flag.StringVar(&separator, "sp", "-", "separator between characters")
 	flag.StringVar(&inputPath, "in", "", "input path")
 
-	file2Rename = make(FilePaths, 0)
-	trans = transliterate.Sugar("-", "")
-}
-
-func main() {
 	flag.Parse()
 	if help || len(inputPath) == 0 {
 		flag.Usage()
@@ -73,6 +72,23 @@ func main() {
 		flag.Usage()
 		return
 	}
+}
+
+func askForAnswer() string {
+	var ans string
+	fmt.Scanln(&ans)
+	return strings.ToLower(ans)
+}
+func askForContinue() bool {
+	var yes string
+	fmt.Scanln(&yes)
+	return strings.ToLower(yes) == "y"
+}
+
+func main() {
+	file2Rename = make(FilePaths, 0)
+	trans = transliterate.Sugar(separator, "")
+	colorize = aurora.New()
 	inputPath = filepath.Clean(inputPath)
 	_, err := os.Stat(inputPath)
 	if err != nil {
@@ -82,12 +98,19 @@ func main() {
 
 	filepath.Walk(inputPath, walker)
 	if !isSilent {
-		fmt.Println("There are total", len(file2Rename), "files to rename")
+		fmt.Print("There are total ", colorize.Green(len(file2Rename)), " files to rename\n\n")
+		if !skipComfirm {
+			fmt.Println(colorize.Cyan("Confirm to rename? [y/n]"))
+			if !askForContinue() {
+				return
+			}
+		}
 	}
 
 	if len(file2Rename) > 0 {
+		ignoreError := false
 		sort.Sort(file2Rename)
-		fmtStr := fmt.Sprintf("[%%0%dd] %%s %%s ---> %%s\n", int(math.Ceil(math.Log10(float64(len(file2Rename))))))
+		fmtStr := fmt.Sprintf("[%%s] %%0%dd. %%s %%s%%s -> %%s\n", int(math.Ceil(math.Log10(float64(len(file2Rename)+1)))))
 		for i, v := range file2Rename {
 			var fType string
 			if v.isDir {
@@ -95,13 +118,37 @@ func main() {
 			} else {
 				fType = "F"
 			}
+			var err error
 			if !isDry {
-				if !isSilent {
-					fmt.Printf(fmtStr, i, fType, filepath.Clean(string(v.path)+string(filepath.Separator)+v.oldName), v.newName)
-				}
 				old := filepath.Clean(string(v.path) + string(filepath.Separator) + v.oldName)
 				new := filepath.Clean(string(v.path) + string(filepath.Separator) + v.newName)
-				os.Rename(old, new)
+				err = os.Rename(old, new)
+			}
+			if !isSilent {
+				var result string
+				if isDry {
+					result = "↩️"
+				} else {
+					if err == nil {
+						result = "✅"
+					} else {
+						result = "❌"
+					}
+				}
+				fmt.Printf(fmtStr, result, i+1, colorize.BgWhite(fType).Black(), strings.TrimSuffix(string(v.path), string(filepath.Separator))+string(filepath.Separator), colorize.Yellow(v.oldName).Faint(), colorize.Green(v.newName).Bold())
+			}
+
+			if err != nil {
+				fmt.Println(err)
+				if !ignoreError {
+					fmt.Println(colorize.Red("Continue with error? [Y/N/All]"))
+					switch askForAnswer()[0] {
+					case 'a':
+						ignoreError = true
+					case 'n':
+						return
+					}
+				}
 			}
 		}
 	}
@@ -114,14 +161,14 @@ func walker(realPath string, f os.FileInfo, err error) error {
 
 	if !isSilent {
 		if f.IsDir() {
-			fmt.Printf("[D] %s", realPath)
+			fmt.Printf("[D] %s%c%s", strings.TrimSuffix(filepath.Dir(realPath), string(filepath.Separator)), filepath.Separator, colorize.BrightBlue(oldName+ext))
 			if oldName != newName {
-				fmt.Printf(" -> %s", newName+ext)
+				fmt.Printf(" -> %s", colorize.BrightCyan(newName+ext))
 			}
 			fmt.Printf("\n")
 		} else {
 			if oldName != newName {
-				fmt.Printf("\t[F] %s -> %s\n", oldName+ext, newName+ext)
+				fmt.Printf("\t[F] %s\n\t--> %s\n", colorize.BrightBlue(oldName+ext), colorize.BrightCyan(newName+ext))
 			}
 		}
 	}
